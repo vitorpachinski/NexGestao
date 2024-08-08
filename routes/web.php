fe28879bf57381d47a0a8efc9591fa1c776055a0
Vitor-HenriqueP <engs-vitorpachinski@camporeal.edu.br>
2024-08-08 20:48:48 -0300

    Created new routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobrenos']);
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
-Route::get('/testes', [\App\Http\Controllers\TestesController::class, 'testes']);
+Route::get('/login', function(){
+    return 'Login';
+});
+Route::get('/clientes', function(){
+    return 'Clientes';
+});
+Route::get('/fornecedores', function(){
+    return 'Fornecedores';
+});
+Route::get('/produtos', function(){
+    return 'Produtos';
+});
 
 Route::get(
     '/contato/{nome}/{categoria_id}',
