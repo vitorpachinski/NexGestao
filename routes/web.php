c3b42a1e11a58390b1d02d18a8b046c57c1913db
Vitor-HenriqueP <engs-vitorpachinski@camporeal.edu.br>
2024-08-08 20:58:35 -0300

    grouping routes

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobrenos']);
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
-Route::get('/login', function(){
-    return 'Login';
-});
-Route::get('/clientes', function(){
-    return 'Clientes';
-});
-Route::get('/fornecedores', function(){
-    return 'Fornecedores';
-});
-Route::get('/produtos', function(){
-    return 'Produtos';
+Route::get('/login', function(){return 'Login';});
+Route::prefix('/app')->group(function(){
+    
+    Route::get('/clientes', function(){return 'Clientes';});
+    Route::get('/fornecedores', function(){return 'Fornecedores';});
+    Route::get('/produtos', function(){return 'Produtos';});
+
 });
 
 Route::get(
