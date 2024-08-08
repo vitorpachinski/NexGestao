618ccd6b769956c8516c41ef238a2f9578801e49
Vitor-HenriqueP <engs-vitorpachinski@camporeal.edu.br>
2024-08-08 20:12:57 -0300

    sending parameters with routes

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,10 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal']);
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobrenos']);
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato']);
 Route::get('/testes',[\App\Http\Controllers\TestesController::class,'testes']);
+
+Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}', function(string $nome, int $categoria, string $assunto, string $mensagem){
+    echo 'Estamos aqui, '. $nome;
+    echo ' Categoria '. $categoria;
+    echo ' Assunto: '. $assunto;
+    echo ' Mensagem: '. $mensagem;
+});
