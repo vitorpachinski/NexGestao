8528f4db3f50acbe2bea0a02ac93199bc172a244
Vitor <vitorpachinski@gmail.com>
2025-01-20 20:38:57 -0300

    feat(app/Models): updated models

--- app/Models/SiteContact.php
+++ app/Models/SiteContact.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class SiteContact extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'reason',
+        'message'
+    ];
 }
