f726ac1db6134b09903a5c87f12cb0c1360212ef
Vitor-HenriqueP <engs-vitorpachinski@camporeal.edu.br>
2025-01-24 19:45:37 -0300

    feat(Controllers/ContatoController): added validate method in ContatoController

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -13,15 +13,22 @@ class ContatoController extends Controller
     }
 
     public function store(Request $request){
-        $contact = new SiteContact();
         /*$contact->name = $request->input('name');
         $contact->email = $request->input('email');
         $contact->phone = $request->input('phone');
         $contact->reason = $request->input('reason');
         $contact->message = $request->input('message');
         */
-        $contact->create($request->all());
-        $contact->save();
+        //SiteContact::create($request->all());
+       // SiteContact::save();
+
+       $request->validate([
+        'name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'string', 'email:rfc', 'max:255', 'unique:site_contacts'],
+        'phone' => ['required', 'string', 'max:20'],
+        'reason' => ['required', 'integer'],
+        'message' => ['required', 'string', 'max:500'],
+       ]);
 
     }
 }
--- resources/views/site/components/form_contato.blade.php
+++ resources/views/site/components/form_contato.blade.php
@@ -1,13 +1,13 @@
 {{$slot}}
 <form action="{{route('site.contato.store')}}" method="POST">
     @csrf
-    <input type="text" placeholder="Nome" name="name" class="borda-preta">
+    <input type="text" placeholder="Nome" name="name"  class="borda-preta" >
     <br>
-    <input type="text" placeholder="Telefone" name="phone" class="borda-preta">
+    <input type="text" placeholder="Telefone" name="phone" class="borda-preta" >
     <br>
-    <input type="text" placeholder="E-mail" name="email" class="borda-preta">
+    <input type="text" placeholder="E-mail" name="email" class="borda-preta" >
     <br>
-    <select class="borda-preta" name="reason">
+    <select class="borda-preta" name="reason" >
         <option value="">Qual o motivo do contato?</option>
         <option value="1">Dúvida</option>
         <option value="2">Elogio</option>
@@ -18,3 +18,8 @@
     <br>
     <button type="submit" class="borda-preta">ENVIAR</button>
 </form>
+<div style="position:absolute; top:0px; left: 0px; width: 100%;background:red;">
+    <pre>
+    {{ print_r($errors) }} 
+</pre>
+</div>
