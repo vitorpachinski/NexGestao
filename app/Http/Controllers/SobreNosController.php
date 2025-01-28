1e3ac3bd0dea609d222f56c82067bd37c422c254
Vitor-HenriqueP <engs-vitorpachinski@camporeal.edu.br>
2025-01-28 20:29:46 -0300

    feat(Controllers/SobreNosController): added middleware in controller

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Middleware\AccessLogMiddleware;
 
 class SobreNosController extends Controller
 {
+    public function __construct(){
+        $this->middleware(AccessLogMiddleware::class);
+    }
     public function sobrenos(){
         return view('site.sobre-nos', ['title' => 'Super Gestão - Sobre Nós']);
     }
